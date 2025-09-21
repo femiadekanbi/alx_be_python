84cc4bec3173f1d5b06dd09b582f3cff9134cd7a
Olufemi Adekanbi <femiadekanbi@gmail.com>
2025-09-21 23:24:41 +0100

    Corrected typo error

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for n in range(1, 11):
     product = number * n
-    print(f"{number} _ {n} = {product}")
+    print(f"{number} * {n} = {product}")
